2c452659a8c69cba5e0fa86decfeccd46e203c8c
Vlad_Den <33760265+VladDen4@users.noreply.github.com>
2022-12-16 16:00:08 +0300

    Create Config.cs

---
+++ TGBot/Config.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Vlad_Den <vladden500@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace TGBot
+{
+    /// <summary>
+    /// Bot configuration class.
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// Required field! Contains the bot's access token. You can get it from <see href="https://t.me/BotFather">@BotFather</see>.
+        /// </summary>
+        private static readonly string token = Environment.GetEnvironmentVariable("TG_TOKEN") ?? "token must here";
+
+        /// <summary>
+        /// Read-only parameter.
+        /// </summary>
+        /// <returns>
+        /// The bot's telegram token.
+        /// </returns>
+        public static string Token { get { return token; } }
+    }
+}
